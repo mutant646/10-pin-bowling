35769db3423180ff380cbac60599b176b1b12e6a
LEWIS BROWN <2502272@uad.ac.uk>
2025-11-13 15:07:36 +0000

    refreshed bowling lane

--- Assets/script/rolling.cs
+++ Assets/script/rolling.cs
@@ -1,29 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
-using UnityEngine.InputSystem;
-public class PlayerMovement : MonoBehaviour
+
+public class rolling : MonoBehaviour
 {
     public float speed = 0;
     private Rigidbody rb;
-    InputAction moveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        moveAction = InputSystem.actions.FindAction("Move");
+  rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Vector2 inputdirection = moveAction.ReadValue<Vector2>();
-        float moveHorizontal = inputdirection.x;
-        float moveVertical = inputdirection.y;
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
-
-        float angle = Mathf.Atan2(moveVertical, moveHorizontal) * Mathf.Rad2Deg;
-        Vector3 rotation = new Vector3(0, -angle + 90, 0); //rotate around the z-axis
-        transform.eulerAngles = rotation;
+        bool spaceBarDown = Input.GetKeyDown(KeyCode.Space);
+        
+        Vector3 movement = new Vector3(1, 0.0f, 0.0f);
+        
+        rb.AddForce(speed * movement);
+     
+        
+        if (spaceBarDown == true)
+        {
+            speed = 1.5f;     
+        }
+        
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
 }
